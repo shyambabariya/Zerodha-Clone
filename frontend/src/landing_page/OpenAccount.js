43d3e4d58a9e5888b68e5e63807144a88acf23b2
@MaD Max <shyambabariyaa@gmail.com>
2025-10-30 23:28:46 +0530

    openaccount component modified

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function OpenAccount() {
+    return ( 
+       <div className="container">
+      <div className="row text-center p-5 mt-5">
+        <h1 className="mt-2 fs-3 mb-3">Open a Zerodha account</h1>
+        <p className="text-muted">
+         Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+        </p>
+        <button className="btns p-2 mt-4 fs-5">Sign up for free</button>
+      </div>
+    </div>
+     );
+}
+
+export default OpenAccount;
