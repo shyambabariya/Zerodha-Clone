002f3855ef9587f7c85ba6247488cda4f814587a
@MaD Max <shyambabariyaa@gmail.com>
2025-11-06 23:47:36 +0530

    indexing file added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './index.css';
+import Navbar from "./landing_page/Navbar";
+import HomePage from "./landing_page/home/HomePage";
+import SignUp from "./landing_page/signup/Signup";
+import AboutPage from './landing_page/about/AboutPage';
+import ProductPage from './landing_page/products/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import NotFound from "./landing_page/NotFound";
+import Footer from "./landing_page/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-   <div>Hellow</div>
-  </React.StrictMode>
+    <BrowserRouter>
+    <Navbar/>
+    {/* Here is the Navbar and Footer can include in all files */}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        {/* here the Route is self closing tag so either we use it without </Route> tag or /> */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/product" element={<ProductPage/>}/>
+        <Route path="/pricing" element={<PricingPage/>}/>
+        <Route path="/support" element={<SupportPage/>}/>
+        {/*  * is write after all the routes bcoz this for only the if not found any routes  */}
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
 );
