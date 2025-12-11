5a382848d0e45597a51f7dc0de2c6c742f4e0844
@MaD Max <shyambabariyaa@gmail.com>
2025-12-11 22:05:00 +0530

    hero component added

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,15 +1,15 @@
-import React from 'react'
+// import React from 'react'
 
-function Hero() {
-    return ( 
-       <div className='container mt-5 border-bottom'>
-        <div className='text-center mt-5 mb-5 p-3'>
-        <h2>Zerodha Products</h2>
-        <h4 className='text-muted mt-3'>Sleek, modern, and intuitive trading platforms</h4>
-        <p className='mt-4'>Check out our <a href='#'>investment offerings <i class="fa-solid fa-arrow-right"></i></a></p>
-        </div>
-       </div>
-     );
-}
+// function Hero() {
+//     return ( 
+//        <div className='container mt-5 border-bottom'>
+//         <div className='text-center mt-5 mb-5 p-3'>
+//         <h2>Zerodha Products</h2>
+//         <h4 className='text-muted mt-3'>Sleek, modern, and intuitive trading platforms</h4>
+//         <p className='mt-4'>Check out our <a href='#'>investment offerings <i class="fa-solid fa-arrow-right"></i></a></p>
+//         </div>
+//        </div>
+//      );
+// }
 
-export default Hero;
+// export default Hero;
